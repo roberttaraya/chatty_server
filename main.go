d95ec51e4c23340fb4b3ebca28245414e3a5b216
roberttaraya <robert.taraya@gmail.com>
2016-05-31 15:16:50 -0500

    implement websockets in Go

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
   "fmt"
+  "github.com/gorilla/websocket"
   "net/http"
 )
+
+var upgrader = websocket.Upgrader{
+  ReadBufferSize:   1024,
+  WriteBufferSize:  1024,
+  CheckOrigin:      func(r*http.Request) bool { return true },
+}
 
 func main() {
   http.HandleFunc("/", handler)
@@ -11,5 +18,22 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "hello from go")
+  // fmt.Fprintf(w, "hello from go")
+  socket, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  for {
+    msgType, msg, err := socket.ReadMessage()
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    fmt.Println(string(msg))
+    if err = socket.WriteMessage(msgType, msg); err !=nil {
+      fmt.Println(err)
+      return
+    }
+  }
 }
