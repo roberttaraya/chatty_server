e7568b4d21154eb1cfcfe3b512c3a49fa9bce2fc
roberttaraya <robert.taraya@gmail.com>
2016-05-31 17:58:45 -0500

    create router, handler, and client of Go server

--- client.go
+++ client.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "math/rand"
+  "github.com/gorilla/websocket"
 )
 
 type Message struct {
@@ -11,46 +9,40 @@ type Message struct {
   Data interface{} `json:"data"`
 }
 
+type FindHandler func(string) (Handler, bool)
+
 type Client struct {
-  send chan Message
+  send        chan Message
+  socket      *websocket.Conn
+  findHandler FindHandler
 }
 
-func (client *Client) write() {
+func (client *Client) Read() {
+  var message Message
+  for {
+    if err := client.socket.ReadJSON(&message); err != nil {
+      break
+    }
+    if handler, found := client.findHandler(message.Name); found {
+      handler(client, message.Data)
+    }
+  }
+  client.socket.Close()
+}
+
+func (client *Client) Write() {
   for msg := range client.send {
-    // TODO: socket.sendJSON(msg)
-    fmt.Printf("%#v\n", msg)
+    if err := client.socket.WriteJSON(msg); err != nil {
+      break
+    }
   }
+  client.socket.Close()
 }
 
-func (client *Client) subscribeChannels() {
-  //TODO: changefeed query rethinkDB
-  for {
-    time.Sleep(r())
-    client.send <- Message{"channel add", ""}
-  }
-}
-
-func (client *Client) subscribeMessages() {
-  //TODO: changefeed query rethinkDB
-  for {
-    time.Sleep(r())
-    client.send <- Message{"message add", ""}
-  }
-}
-
-func r() time.Duration {
-  return time.Millisecond * time.Duration(rand.Intn(1000))
-}
-
-func NewClient() *Client {
+func NewClient(socket *websocket.Conn, findHandler FindHandler) *Client {
   return &Client{
-    send: make(chan Message),
+    send:         make(chan Message),
+    socket:       socket,
+    findHandler:  findHandler,
   }
-}
-
-func main() {
-  client := NewClient()
-  go client.subscribeChannels()
-  go client.subscribeMessages()
-  client.write()
 }
--- main.go
+++ main.go
@@ -1,95 +1,19 @@
 package main
 
 import (
-  "fmt"
-  "github.com/gorilla/websocket"
   "net/http"
-  "github.com/mitchellh/mapstructure"
-  "time"
 )
-
-type Message struct {
-  Name string      `json:"name"`
-  Data interface{} `json:"data"`
-}
 
 type Channel struct {
   Id string   `json:"id"`
   Name string `json:"name"`
 }
 
-var upgrader = websocket.Upgrader{
-  ReadBufferSize:   1024,
-  WriteBufferSize:  1024,
-  CheckOrigin:      func(r*http.Request) bool { return true },
-}
-
 func main() {
-  http.HandleFunc("/", handler)
+  router := NewRouter()
+
+  router.Handle("channel add", addChannel)
+
+  http.Handle("/", router)
   http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-  // fmt.Fprintf(w, "hello from go")
-  socket, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  for {
-    // msgType, msg, err := socket.ReadMessage()
-    // if err != nil {
-    //   fmt.Println(err)
-    //   return
-    // }
-    var inMessage Message
-    var outMessage Message
-    if err := socket.ReadJSON(&inMessage); err != nil {
-      fmt.Println(err)
-      break
-    }
-    fmt.Printf("%#v\n", inMessage)
-    switch inMessage.Name {
-      case "channel add":
-        err := addChannel(inMessage.Data)
-        if err != nil {
-          outMessage = Message{"error", err}
-          if err := socket.WriteJSON(outMessage); err != nil {
-            fmt. Println(err)
-            break
-          }
-        }
-      case "channel subscribe":
-        go subscribeChannel(socket)
-    }
-    // fmt.Println(string(msg))
-    // if err = socket.WriteMessage(msgType, msg); err !=nil {
-    //   fmt.Println(err)
-    //   return
-    // }
-  }
-}
-
-func addChannel(data interface{}) (error) {
-  var channel Channel
-
-  err := mapstructure.Decode(data, &channel)
-  if err != nil {
-    return err
-  }
-  channel.Id = "1"
-  // fmt.Printf("%#v\n", channel)
-  fmt.Println("added channel")
-  return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-  // TODO: rethinkDB query / changefeed
-  for {
-    time.Sleep(time.Second * 1)
-    message := Message{"channel add",
-      Channel{"1", "software Support"}}
-    socket.WriteJSON(message)
-    fmt.Println("sent new channel")
-  }
 }
