81933186d4ee17ee10a70c7ae26fbb201941876a
roberttaraya <robert.taraya@gmail.com>
2016-05-31 16:24:23 -0500

    handle channel related app messages

--- main.go
+++ main.go
@@ -4,7 +4,19 @@ import (
   "fmt"
   "github.com/gorilla/websocket"
   "net/http"
+  "github.com/mitchellh/mapstructure"
+  "time"
 )
+
+type Message struct {
+  Name string      `json:"name"`
+  Data interface{} `json:"data"`
+}
+
+type Channel struct {
+  Id string   `json:"id"`
+  Name string `json:"name"`
+}
 
 var upgrader = websocket.Upgrader{
   ReadBufferSize:   1024,
@@ -25,15 +37,59 @@ func handler(w http.ResponseWriter, r *http.Request) {
     return
   }
   for {
-    msgType, msg, err := socket.ReadMessage()
-    if err != nil {
+    // msgType, msg, err := socket.ReadMessage()
+    // if err != nil {
+    //   fmt.Println(err)
+    //   return
+    // }
+    var inMessage Message
+    var outMessage Message
+    if err := socket.ReadJSON(&inMessage); err != nil {
       fmt.Println(err)
-      return
+      break
     }
-    fmt.Println(string(msg))
-    if err = socket.WriteMessage(msgType, msg); err !=nil {
-      fmt.Println(err)
-      return
+    fmt.Printf("%#v\n", inMessage)
+    switch inMessage.Name {
+      case "channel add":
+        err := addChannel(inMessage.Data)
+        if err != nil {
+          outMessage = Message{"error", err}
+          if err := socket.WriteJSON(outMessage); err != nil {
+            fmt. Println(err)
+            break
+          }
+        }
+      case "channel subscribe":
+        go subscribeChannel(socket)
     }
+    // fmt.Println(string(msg))
+    // if err = socket.WriteMessage(msgType, msg); err !=nil {
+    //   fmt.Println(err)
+    //   return
+    // }
+  }
+}
+
+func addChannel(data interface{}) (error) {
+  var channel Channel
+
+  err := mapstructure.Decode(data, &channel)
+  if err != nil {
+    return err
+  }
+  channel.Id = "1"
+  // fmt.Printf("%#v\n", channel)
+  fmt.Println("added channel")
+  return nil
+}
+
+func subscribeChannel(socket *websocket.Conn) {
+  // TODO: rethinkDB query / changefeed
+  for {
+    time.Sleep(time.Second * 1)
+    message := Message{"channel add",
+      Channel{"1", "software Support"}}
+    socket.WriteJSON(message)
+    fmt.Println("sent new channel")
   }
 }
